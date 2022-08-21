e9a949a012f6096a29d9c381763cd0e2bd32e191
Ryan Werner <crusader8494@gmail.com>
2022-08-21 16:39:42 -0400

    initial commit

---
+++ AES-256/AES-256/AES_256_main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include "AES_256_main.h"
+
+void AES::AESEncrpyt(std::vector<char> inputState)
+{
+	AES::UnpackInputState();
+}
+
+std::vector<char> AES::AESDecrpyt()
+{
+
+}
+
+void AES::UnpackInputState()
+{
+	
+}
+
+void AES::PackOutputState()
+{
+
+}
+
+void AES::ExpandKey()
+{
+
+}
+
+void AES::NormalRounds()
+{
+
+}
+
+void AES::LastRound()
+{
+
+}
+
+void AES::SBox()
+{
+
+}
+
+void AES::ShiftRows()
+{
+
+}
+
+void AES::MixColumns()
+{
+
+}
+
+void AES::AddRoundKey()
+{
+
+}
---
+++ AES-256/AES-256/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "main.h"
+#include "AES_256_main.h"
+
+int main() {
+
+	return 0;
+}
